14a355d057a887afbfe3627f08ebd435243a73fa
keagnon <keagnon.gbe@gmail.com>
2024-08-01 15:12:07 +0200

    Adding list of librairies

--- scripts/financial_analysis.py
+++ scripts/financial_analysis.py
@@ -1,3 +1,4 @@
+""" Analysis"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -31,21 +32,26 @@ df_economies = pd.DataFrame(donnees_economies)
 fig, ax = plt.subplots(2, 1, figsize=(14, 14))
 
 # Coûts directs et indirects
-df_euros.plot(kind="bar", x="Catégorie", y=["Coûts directs (EUR)", "Coûts indirects (EUR)"], ax=ax[0], color=["orange", "red"])
+df_euros.plot(
+    kind="bar", x="Catégorie",
+    y=["Coûts directs (EUR)",
+    "Coûts indirects (EUR)"],
+    ax=ax[0], color=["orange", "red"])
 ax[0].set_title("Analyse des Coûts Directs et Indirects")
 ax[0].set_ylabel("Coût en EUR")
 ax[0].set_xlabel("Catégories de Coûts")
 ax[0].legend(["Coûts directs", "Coûts indirects"])
 
 # Économies estimées
-df_economies.plot(kind="bar", x="Catégorie", y="Économies estimées (EUR)", ax=ax[1], color='green')
+df_economies.plot(kind="bar", x="Catégorie",
+y="Économies estimées (EUR)", ax=ax[1], color='green')
 ax[1].set_title("Économies Potentielles Grâce à la Détection des Anomalies")
 ax[1].set_ylabel("Économies en EUR")
 ax[1].set_xlabel("Catégories de Coûts")
 ax[1].legend(["Économies estimées"])
 
 plt.tight_layout()
-
 plt.savefig("analyse_financiere.png")
-
 plt.show()
+
+
