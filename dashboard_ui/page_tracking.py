c9af0b6a841df0fa50e0a5711a3f6cb6149d49ce
keagnon <keagnon.gbe@gmail.com>
2024-09-12 15:46:34 +0200

    Refactor page_tracking

--- dashboard_ui/page_tracking.py
+++ dashboard_ui/page_tracking.py
@@ -1,22 +1,43 @@
-import streamlit as st
-import webbrowser
-import os
+"""
+Module pour afficher la page de tracking et ouvrir l'interface utilisateur de MLflow.
+"""
 
+import os
+import webbrowser
+import streamlit as st
 from dotenv import load_dotenv
 
+# Charger les variables d'environnement
 load_dotenv()
 
 def local_css(file_name):
-    with open(file_name) as f:
+    """
+    Charge et applique un fichier CSS pour styliser l'application.
+
+    Args:
+        file_name (str): Chemin vers le fichier CSS.
+    """
+    with open(file_name, 'r', encoding='utf-8') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 local_css("styles.css")
 
 
-# Fonction pour afficher la page de tracking
 def open_mlflow_ui():
+    """
+    Ouvre l'interface utilisateur MLflow dans un nouvel onglet du navigateur.
+    """
     url = os.getenv("MLFLOW_TRACKING_URI")
-    webbrowser.open_new_tab(url)
+    if url:
+        webbrowser.open_new_tab(url)
+    else:
+        st.error("L'URL de MLflow n'est pas définie.")
+
 
 def show_tracking_page():
-    st.title("Tracking........")
+    """
+    Affiche la page de tracking dans l'application Streamlit.
+    """
+    st.title("Tracking des Expériences MLflow")
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.write("Vous pouvez suivre les métriques et les performances des modèles ici.")
---
+++ dashboard_ui/utils.py
@@ -0,0 +1,23 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def local_css(file_name):
+    """
+    Charge et applique un fichier CSS pour styliser l'application.
+
+    Args:
+        file_name (str): Chemin vers le fichier CSS.
+    """
+    with open(file_name, 'r', encoding='utf-8') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def configure_google_credentials():
+    """
+    Configure les credentials Google Cloud à partir des variables d'environnement.
+    """
+    google_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    if google_credentials:
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials
