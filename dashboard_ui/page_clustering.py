3e2fb7b18347ee447f42c9162a1054d15b33bbb9
keagnon <keagnon.gbe@gmail.com>
2024-09-12 15:22:38 +0200

    Refactor: Corrections Pylint page_clustering

--- dashboard_ui/page_clustering.py
+++ dashboard_ui/page_clustering.py
@@ -11,7 +11,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import plotly.express as px
 from dotenv import load_dotenv
-import plotly.graph_objects as go
 
 load_dotenv()
 
@@ -66,7 +65,7 @@ def log_cluster_metrics(unique_labels, counts):
         unique_labels (np.ndarray): Labels uniques des clusters.
         counts (np.ndarray): Nombre de points dans chaque cluster.
     """
-    with mlflow.start_run() as run:
+    with mlflow.start_run():
         for label, count in zip(unique_labels, counts):
             if label == -1:
                 mlflow.log_metric('noise_points', count)
@@ -85,9 +84,102 @@ def display_cluster_summary(unique_labels, counts):
     st.subheader("Résumé des clusters")
     for label, count in zip(unique_labels, counts):
         if label == -1:
-            st.markdown(f"<span style='color: red;'>• Cluster {label} : {count} points</span>", unsafe_allow_html=True)
+            st.markdown(
+                f"<span style='color: red;'>• Cluster {label} : {count} points</span>",
+                unsafe_allow_html=True
+            )
         else:
-            st.markdown(f"<span style='color: green;'>• Cluster {label} : {count} points</span>", unsafe_allow_html=True)
+            st.markdown(
+                f"<span style='color: green;'>• Cluster {label} : {count} points</span>",
+                unsafe_allow_html=True
+            )
+
+
+def display_clustering_plot(df, hourly_columns):
+    """
+    Affiche le graphe du clustering en utilisant la réduction de dimensions PCA.
+
+    Args:
+        df (pd.DataFrame): DataFrame contenant les données prétraitées.
+        hourly_columns (list): Liste des colonnes horaires.
+    """
+    scaler = StandardScaler()
+    x_data = scaler.fit_transform(df[hourly_columns])
+    df['cluster'] = loaded_model.fit_predict(x_data)
+
+    # Réduction de dimensions pour visualisation
+    pca = PCA(n_components=2)
+    df_pca = pca.fit_transform(x_data)
+    df['pca1'] = df_pca[:, 0]
+    df['pca2'] = df_pca[:, 1]
+
+    unique_labels, counts = np.unique(df['cluster'], return_counts=True)
+
+    # Enregistrer les métriques dans MLflow
+    log_cluster_metrics(unique_labels, counts)
+
+    col1, col2 = st.columns([7, 3], gap="medium")
+
+    with col1:
+        fig = px.scatter(
+            df,
+            x='pca1',
+            y='pca2',
+            color='cluster',
+            title="Clustering DBSCAN",
+            labels={'pca1': 'PCA 1', 'pca2': 'PCA 2', 'cluster': 'Cluster'},
+            hover_data=['date', 'région', 'consommation_moyenne_journalière', 'cluster'],
+            size=np.ones(len(df)) * 10
+        )
+        fig.update_layout(coloraxis_colorbar={'title': 'Cluster'})
+        st.plotly_chart(fig)
+        st.markdown(
+            '<div style="text-align: center;"><em>Visualisation des clusters</em></div>',
+            unsafe_allow_html=True
+        )
+
+    with col2:
+        display_cluster_summary(unique_labels, counts)
+
+    return df, unique_labels
+
+
+def display_noise_points(df, unique_labels):
+    """
+    Afficher les points considérés comme bruit dans les clusters.
+
+    Args:
+        df (pd.DataFrame): DataFrame contenant les données prétraitées.
+        unique_labels (np.ndarray): Labels uniques des clusters.
+    """
+    col3, col4 = st.columns([7, 3], gap="medium")
+
+    with col3:
+        noise_points = df[df['cluster'] == -1]
+        if not noise_points.empty:
+            fig_noise = px.scatter(
+                noise_points,
+                x='pca1',
+                y='pca2',
+                title="Points marqués comme bruit",
+                labels={'pca1': 'PCA 1', 'pca2': 'PCA 2'},
+                hover_data=['date', 'région'],
+                size=np.ones(len(noise_points)) * 10,
+                color_discrete_sequence=['red']
+            )
+            st.plotly_chart(fig_noise)
+            st.markdown(
+                '<div style="text-align: center;"><em>Visualisation des points de bruit</em></div>',
+                unsafe_allow_html=True
+            )
+
+    with col4:
+        if not noise_points.empty:
+            st.dataframe(noise_points[['date', 'région', 'cluster']])
+            st.markdown(
+                '<div style="text-align: center;"><em>Tableau des points de bruit</em></div>',
+                unsafe_allow_html=True
+            )
 
 
 def show_clustering():
@@ -106,70 +198,9 @@ def show_clustering():
     if uploaded_file is not None:
         df, hourly_columns = preprocess_data(uploaded_file)
 
-        # Appliquer le modèle DBSCAN
         if all(column in df.columns for column in hourly_columns):
-            scaler = StandardScaler()
-            x_data = scaler.fit_transform(df[hourly_columns])
-            df['cluster'] = loaded_model.fit_predict(x_data)
-
-            # Réduction de dimensions pour visualisation
-            pca = PCA(n_components=2)
-            df_pca = pca.fit_transform(x_data)
-            df['pca1'] = df_pca[:, 0]
-            df['pca2'] = df_pca[:, 1]
-
-            # Compter le nombre de points dans chaque cluster
-            unique_labels, counts = np.unique(df['cluster'], return_counts=True)
-
-            # Enregistrer les métriques dans MLflow
-            log_cluster_metrics(unique_labels, counts)
-
-            col1, col2 = st.columns([7, 3], gap="medium")
-
-            with col1:
-                fig = px.scatter(
-                    df,
-                    x='pca1',
-                    y='pca2',
-                    color='cluster',
-                    title="Clustering DBSCAN",
-                    labels={'pca1': 'PCA 1', 'pca2': 'PCA 2', 'cluster': 'Cluster'},
-                    hover_data=['date', 'région', 'consommation_moyenne_journalière', 'cluster'],
-                    size=np.ones(len(df)) * 10
-                )
-                fig.update_layout(coloraxis_colorbar=dict(title="Cluster"))
-                st.plotly_chart(fig)
-                st.markdown('<div style="text-align: center;"><em>Visualisation des clusters</em></div>', unsafe_allow_html=True)
-
-            with col2:
-                display_cluster_summary(unique_labels, counts)
-
-            st.markdown("<br>", unsafe_allow_html=True)
-            st.markdown("---")
-            col3, col4 = st.columns([7, 3], gap="medium")
-
-            with col3:
-                # Visualisation des points de bruit
-                noise_points = df[df['cluster'] == -1]
-                if not noise_points.empty:
-                    fig_noise = px.scatter(
-                        noise_points,
-                        x='pca1',
-                        y='pca2',
-                        title="Points marqués comme bruit",
-                        labels={'pca1': 'PCA 1', 'pca2': 'PCA 2'},
-                        hover_data=['date', 'région'],
-                        size=np.ones(len(noise_points)) * 10,
-                        color_discrete_sequence=['red']
-                    )
-                    st.plotly_chart(fig_noise)
-                    st.markdown('<div style="text-align: center;"><em>Visualisation des points de bruit</em></div>', unsafe_allow_html=True)
-
-            with col4:
-                if not noise_points.empty:
-                    st.dataframe(noise_points[['date', 'région', 'cluster']])
-                    st.markdown('<div style="text-align: center;"><em>Tableau des points de bruit</em></div>', unsafe_allow_html=True)
-
+            df, unique_labels = display_clustering_plot(df, hourly_columns)
+            display_noise_points(df, unique_labels)
         else:
             st.error("Les colonnes horaires ne sont pas toutes présentes dans le dataset.")
     else:
