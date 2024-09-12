0ebd8872c6f6cf333e5866aa02c13de8160bc047
keagnon <keagnon.gbe@gmail.com>
2024-09-12 14:34:44 +0200

    Refactor: Amélioration du code selon les recommandations Pylint
    - Ajout de docstrings pour les modules et fonctions
    - Correction de l'ordre des imports
    - Ajout de l'encodage explicite lors de l'ouverture de fichiers
    - Correction d'une faute de frappe dans l'option du menu (Prédiction Consommation)
    - Suppression des imports inutilisés
    - Ajout d'une nouvelle ligne à la fin du fichier pour respecter les conventions PEP8

--- dashboard_ui/app.py
+++ dashboard_ui/app.py
@@ -1,8 +1,14 @@
+"""
+Application Streamlit pour l'analyse de la consommation d'énergie et la détection d'anomalies.
+Ce module définit la structure de l'interface utilisateur et charge les pages en fonction de la sélection.
+"""
+
+import os
+from dotenv import load_dotenv
+
 import streamlit as st
 from streamlit_option_menu import option_menu
 import mlflow
-import mlflow.sklearn
-import os
 
 from page_anomalie_detection import show_anomalie_detection
 from page_clustering import show_clustering
@@ -11,12 +17,16 @@ from page_prediction_conso import show_prediction_conso
 from page_prediction_meteo import show_prediction_meteo
 from page_tracking import open_mlflow_ui, show_tracking_page
 
-from dotenv import load_dotenv
-
 load_dotenv()
 
 def local_css(file_name):
-    with open(file_name) as f:
+    """
+    Charge et applique un fichier CSS pour styliser l'application.
+
+    Args:
+        file_name (str): Le chemin vers le fichier CSS.
+    """
+    with open(file_name, 'r', encoding='utf-8') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 local_css("styles.css")
@@ -26,7 +36,6 @@ google_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 if google_credentials:
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials
 
-
 # structure de la page
 with st.sidebar:
     selected = option_menu(
@@ -34,7 +43,7 @@ with st.sidebar:
         options=[
             "Prédiction Météo",
             "Anomalie Détection",
-            "Prédiction consonsomation",
+            "Prédiction Consommation",
             "Clustering",
             "Feedback",
             "Tracking"
@@ -65,7 +74,7 @@ if selected == "Prédiction Météo":
     show_prediction_meteo()
 elif selected == "Anomalie Détection":
     show_anomalie_detection()
-elif selected == "Prédiction consonsomation":
+elif selected == "Prédiction Consommation":
     show_prediction_conso()
 elif selected == "Clustering":
     show_clustering()
