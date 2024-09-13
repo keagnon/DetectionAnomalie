158cbf28b4dfd19c56f54d3e9b70ccbd029611be
Melissa <adibmelissa@gmail.com>
2024-09-13 16:18:06 +0200

    ajout des docstings

--- meteo/src/meteo/pipelines/data_processing/pipeline.py
+++ meteo/src/meteo/pipelines/data_processing/pipeline.py
@@ -2,6 +2,19 @@ from kedro.pipeline import Pipeline, node
 from .nodes import preprocess_energy_data
 
 def create_pipeline(**kwargs) -> Pipeline:
+    """
+    Crée un pipeline Kedro qui exécute le prétraitement des données énergétiques.
+
+    Le pipeline se compose d'un seul nœud qui applique la fonction `preprocess_energy_data`
+    pour nettoyer et transformer les données énergétiques.
+
+    Args:
+        **kwargs: Arguments supplémentaires pouvant être fournis pour configurer le pipeline.
+
+    Returns:
+        Pipeline: Un objet `Pipeline` de Kedro, comprenant un nœud qui exécute la fonction 
+        `preprocess_energy_data`.
+    """
     return Pipeline(
         [
             node(
